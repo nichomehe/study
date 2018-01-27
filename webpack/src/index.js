35b770cd5bbe29322dd63f2fdb3498b059e3fff1
nichomehe <1633561235@qq.com>
2018-01-27 20:37:05 +0800

    webpack

---
+++ webpack/src/index.js
@@ -0,0 +1,7 @@
+import css from './css/index.css';
+import sass from './css/common.scss';
+document.write('heheheh');
+let add = (x,y) => x+y;
+let cc = (x,y)=>x*y;
+
+// $('.aa').css('background',"red");
