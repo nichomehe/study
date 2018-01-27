7b2787e86ad0546cdd549f412893d7718f0155e5
nichomehe <1633561235@qq.com>
2018-01-27 20:37:36 +0800

    webpack

---
+++ webpack/dist/index.js
@@ -0,0 +1,42 @@
+/*! 何云莲所有。。。。。 */
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _index = __webpack_require__(1);
+
+var _index2 = _interopRequireDefault(_index);
+
+var _common = __webpack_require__(2);
+
+var _common2 = _interopRequireDefault(_common);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+document.write('heheheh');
+var add = function add(x, y) {
+  return x + y;
+};
+var cc = function cc(x, y) {
+  return x * y;
+};
+
+// $('.aa').css('background',"red");
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+],[0]);
